eaa102e5cc91cc0e6465ebfe206d73462327ec0f
anushkachouhan029 <anushkachouhan029>
2022-12-23 17:02:54 +0530

    added exception for invalid mood

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Program");
+        }
+    }
+
+    public class MoodAnalyzer
+    {
+        string message = string.Empty;
+
+        public MoodAnalyzer()
+        {
+
+        }
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                var mood1 = message.Contains("Happy", StringComparison.OrdinalIgnoreCase);
+                if (mood1)
+                {
+                    return "HAPPY";
+                }
+                var mood2 = message.Contains("Sad", StringComparison.OrdinalIgnoreCase);
+                if (mood2)
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
