1aeb165e36b1e99f1df9198d33ef9ef4f706f79c
anushkachouhan029 <anushkachouhan029>
2022-12-23 16:15:44 +0530

    added uc1 respond happy or sad

---
+++ Testcases/moodtester.cs
@@ -0,0 +1,16 @@
+﻿namespace TestCases
+{
+    [TestClass]
+    public class MoodTester
+    {
+        [TestMethod]
+        public void TestHappyOrSad()
+        {
+            MoodAnalyzerProblem.MoodAnalyzer objMood = new MoodAnalyzerProblem.MoodAnalyzer("Happy"); // Arrange
+
+            string result = objMood.AnalyzeMood(); // Act
+
+            Assert.AreEqual("Happy".ToUpper(), result); //Assert
+        }
+    }
+}
